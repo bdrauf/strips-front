c1be6bb0d5659956e2152f21c6ee4868335ebd8a
bdrauf <rauf.osd19@gmail.com>
2023-11-28 17:18:36 +0700

    api

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -20,7 +20,7 @@ export const Menu = ({ cat }) => {
       <h1>Other post you may like</h1>
       {posts.map((post) => (
         <div className="post" key={post.id}>
-          <img src={`../upload/${post.img}`} alt="" />
+          <img src={post.img} alt="" />
           <h2>{post.title}</h2>
           <button>Read More</button>
         </div>
