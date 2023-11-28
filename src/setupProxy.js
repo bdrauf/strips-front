824b5e8647901ecda1afc893a1f5fad61ec88119
bdrauf <rauf.osd19@gmail.com>
2023-11-28 19:17:43 +0700

    vercel

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.API,
+      target: process.env.local,
       changeOrigin: true,
     }),
   );
